1b4f389e1048bee1e76ad681e887ae69acb93cba
ahtikorhonen <ahti.korhonen@aalto.fi>
2025-03-27 11:13:29 +0200

    implemented simulation class

--- simulation.py
+++ simulation.py
@@ -8,12 +8,10 @@ class Simulation:
     """
     TODO: document
     """
-    def __init__(self, asset: Asset, threats: list[Threat]):
+    def __init__(self, asset: Asset, threats: list[Threat], dt: int = 10 ):
+        self.dt = dt
         self.asset = asset
         self.threats = threats
-        
-    def __initialisize_simulation(self):
-        ...
     
     def num_of_drones(self, drone_type: str) -> int:
         """
@@ -32,14 +30,51 @@ class Simulation:
         
     def simulate_one_attack(self) -> tuple[int, bool]:
         """
-        :return (int, bool): the value of the asset and True if the asset was destroyed, zero and False otherwise
+        :return (int, bool): the value of the asset if the asset was destroyed and zero otherwise
         """
-        pass
+        while self.asset.is_alive:
+            
+            # if all threats are destroyed, return zero 
+            if all([not t.is_alive for t in self.threats]):
+                return 0
+            
+            for threat in self.threats:
+                
+                if not threat.is_alive:
+                    continue
+                
+                # try to spot the threats
+                for observer in self.asset.observers:
+                    observer.spot(threat)
+                    
+                # if spotted - try to destroy the threats
+                for effector in self.asset.effectors:
+                    effector.effect(threat)
+                    
+                # try to destroy the asset
+                if threat.attack_asset():
+                    self.asset.is_alive = False
+            
+                # advance threats
+                threat.update_position(self.asset, self.dt)
+        
+        return self.asset.value
+            
     
     def simulate_n_attacks(self, n: int = 1_000_000) -> tuple[float, float]:
         """
         :return (float, float, float): return the average cost of assets destroyed in one attack,
             standard deviation of costs and frequency/probability of asset getting destroyed 
         """
-        pass
+        costs = []
+        number_assets_destroyed = 0
+        
+        for _ in range(n):
+            cost = self.simulate_one_attack()
+            costs.append(cost)
+            
+            if cost > 0:
+                number_assets_destroyed += 1
+        
+        return np.sum(costs) / n, np.std(costs), number_assets_destroyed / n 
     
