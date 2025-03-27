d3744187d446d187ad3b5a7f669255a746eb4a78
ahtikorhonen <ahti.korhonen@aalto.fi>
2025-03-27 11:13:49 +0200

    implemented threat class

--- threat.py
+++ threat.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 import numpy as np
 
+from asset import Asset
+
 
 class Threat(Enum):
     FPV = "FPV"
@@ -10,7 +12,7 @@ class Threat(Enum):
 
 class Threat:
     """
-    TODO: document
+    A base class for all threats i.e
     """
     def __init__(self, type: Threat, prob: list[tuple], dist: list[float], range: int, speed: int):
         self.type = type
@@ -23,11 +25,26 @@ class Threat:
         self.is_spotted = False
         
     def randomize_initial_position(self) -> tuple[int, int]:
-        position = np.random.randint(5_000, 10_000, 2)
-        while np.sqrt(sum([cord**2 for cord in position])) < 5_000:
+        """
+        Randomly generates x- and y-coordinates such that the initial position is at least
+        5 km from the asset
+        """
+        position = np.random.randint(-10_000, 10_000, 2)
+        while np.sqrt(sum([coord**2 for coord in position])) < 5_000:
             position = np.random.randint(5_000, 10_000, 2)
         
         return position
+    
+    def update_position(self, asset: Asset, dt: int) -> None:
+        """
+        Advances the threats position in the direction of the closest asset
+        :asset (Asset): the closest asset to the threat
+        :dt (int): time interval that passes in a single loop of the simulation
+        """
+        pass
+    
+    def calculate_distance_to_asset(self, asset: Asset) -> None:
+        pass
         
     def attack_asset(self) -> bool:
         """
@@ -35,8 +52,10 @@ class Threat:
         or a failed attack if result is False 
         :return (bool): True if asset was destroyed or False if attack was failed (drone miss/asset not destroyed)
         """
-        if self.dist <= 0:
+        if self.dist and self.is_alive <= 0:
+            self.is_alive = False
+            
             return bool(np.random.binomial(1, self.prob))
-
-if __name__ == "__main__":
-    print(np.random.randint(5_000, 10_000, 2))
+        
+        return False
+    
