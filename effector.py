7de98d8001fe18b0aa075018971d58494d110328
ahtikorhonen <ahti.korhonen@aalto.fi>
2025-03-27 11:14:12 +0200

    implemented effector class

--- effector.py
+++ effector.py
@@ -1,9 +1,18 @@
+import numpy as np
+
 from threat import Threat
 
 
 class Effector:
     def __init__(self, dist: list[float]):
-        self.dist = dist
+        self.dist = dist # [[5000, 0], [3000, 0.1], [2000, 0.2], ...]
+        
+    def get_prob(self, threat_dist) -> float:
+        for dist, prob in self.dist:
+            if dist >= threat_dist:
+                return prob
+        
+        return 0
         
     def effect(self, threat: Threat):
         """
@@ -13,4 +22,7 @@ class Effector:
         :return (None): returns None, in case the threat is destroyed the is_alive attribute of the threat is
                         changed to False
         """
-        ...
+        if threat.is_spotted:
+            destruction_prob = self.get_prob(threat.dist)
+            if np.random.rand() < destruction_prob:
+                threat.is_alive = False
