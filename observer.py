974ae7b21d6c1316f9f7b32251f439e61a741fa5
ahtikorhonen <ahti.korhonen@aalto.fi>
2025-03-27 11:14:27 +0200

    implemented observer class

--- observer.py
+++ observer.py
@@ -1,9 +1,18 @@
+import numpy as np
+
 from threat import Threat
 
 
-class Observe:
+class Observer:
     def __init__(self, dist: list[float]):
         self.dist = dist
+        
+    def get_prob(self, threat_dist) -> float:
+        for dist, prob in self.dist:
+            if dist >= threat_dist:
+                return prob
+        
+        return 0
         
     def spot(self, threat: Threat) -> None:
         """
@@ -13,4 +22,6 @@ class Observe:
         :return (None): returns None, in case the threat is spotted the is_spotted attribute of the threat is
                         changed to true
         """
-        ...
+        spotting_prob = self.get_prob(threat.dist)
+        if np.random.rand() < spotting_prob:
+            threat.is_spotted = True
