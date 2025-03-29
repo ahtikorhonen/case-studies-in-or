7b8ede4640b6224df34797dfd33d24c09fde5f64
ahtikorhonen <ahti.korhonen@aalto.fi>
2025-03-29 23:07:36 +0200

    implemented spot/destroy probability as continuous

--- effector.py
+++ effector.py
@@ -1,28 +1,37 @@
 import numpy as np
 
-from threat import Threat
-
 
 class Effector:
-    def __init__(self, dist: list[float]):
-        self.dist = dist # [[5000, 0], [3000, 0.1], [2000, 0.2], ...]
+    def __init__(self, type: str, value: int, parameters: dict):
+        self.type = type
+        self.value = value
+        self.parameters = parameters
         
-    def get_prob(self, threat_dist) -> float:
-        for dist, prob in self.dist:
-            if dist >= threat_dist:
-                return prob
-        
-        return 0
-        
-    def effect(self, threat: Threat):
+    def get_p(self, threat) -> float:
         """
-        Tries to destroy the threat by sampling from the dist attribute, which represents
-        the discrete probability distribution of the threat getting destroyed as a function of
-        distance to the asset.
+        Returns the probability of succesfully effecting on the threat
+        as a function of distance to the asset/effector
+        """
+        a, b, c, xmin, xmax = self.parameters[threat.type].values()
+        x = threat.distance_to_asset / 1_000
+        
+        if x <= xmin:
+            p = 0.9
+        elif x >= xmax:
+            p = 0
+        else:
+            p = a * x ** 2 + b * x + c
+        
+        return p
+        
+    def effect(self, threat):
+        """
+        Tries to destroy the threat by sampling from the binomial attribute, which represents
+        the probability of the threat getting destroyed as a function of distance to the asset.
         :return (None): returns None, in case the threat is destroyed the is_alive attribute of the threat is
                         changed to False
         """
         if threat.is_spotted:
-            destruction_prob = self.get_prob(threat.dist)
-            if np.random.rand() < destruction_prob:
+            p = self.get_p(threat)
+            if bool(np.random.binomial(1, p)):
                 threat.is_alive = False
--- observer.py
+++ observer.py
@@ -1,20 +1,29 @@
 import numpy as np
 
-from threat import Threat
-
-
 class Observer:
-    def __init__(self, dist: list[float]):
-        self.dist = dist
+    def __init__(self, type: str, value: int, parameters: dict):
+        self.type = type
+        self.value = value
+        self.parameters = parameters
+            
+    def get_p(self, threat) -> float:
+        """
+        Returns the probability of succesfully spotting the threat
+        as a function of distance to the asset/observer
+        """
+        a, b, c, xmin, xmax = self.parameters[threat.type].values()
+        x = threat.distance_to_asset / 1_000
         
-    def get_prob(self, threat_dist) -> float:
-        for dist, prob in self.dist:
-            if dist >= threat_dist:
-                return prob
+        if x <= xmin:
+            p = 0.9
+        elif x >= xmax:
+            p = 0
+        else:
+            p = a * x ** 2 + b * x + c
         
-        return 0
+        return p
         
-    def spot(self, threat: Threat) -> None:
+    def spot(self, threat):
         """
         Tries to spot the threat by sampling from the dist attribute, which represents
         the discrete probability distribution of the threat getting spotted as a function of
@@ -22,6 +31,6 @@ class Observer:
         :return (None): returns None, in case the threat is spotted the is_spotted attribute of the threat is
                         changed to true
         """
-        spotting_prob = self.get_prob(threat.dist)
-        if np.random.rand() < spotting_prob:
+        p = self.get_p(threat)
+        if bool(np.random.binomial(1, p)):
             threat.is_spotted = True
