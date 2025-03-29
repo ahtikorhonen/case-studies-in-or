e0560f2c8e454e1b85a22b15b935f276371dc42e
ahtikorhonen <ahti.korhonen@aalto.fi>
2025-03-29 23:06:57 +0200

    added total_value property

--- asset.py
+++ asset.py
@@ -1,10 +1,21 @@
+from effector import Effector
+from observer import Observer
+
+
 class Asset:
     """
     TODO: document
     """
-    def __init__(self, value: int, effectors: list, observers: list, position: list[int]):
+    def __init__(self, value: int, effectors: list[Effector], observers: list[Observer]):
         self.value = value
         self.effectors = effectors
         self.observers = observers
-        self.position = position
+        self.position = (0,0)
         self.is_alive = True
+    
+    @property    
+    def total_value(self):
+        effectors_value = sum([eff.value for eff in self.effectors])
+        observers_value = sum([obs.value for obs in self.observers])
+        
+        return effectors_value + observers_value + self.value
